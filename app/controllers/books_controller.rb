54ad00c26c0d6cd594b22a088937a94cfe214d22
Joel Caprani <joelcaprani@Joels-MacBook-Air.local>
2017-02-11 11:51:01 -0500

    add respond_to block to controller and create index.html.erb

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,13 @@
 class BooksController < ApplicationController
 
+  def index
+    @books = Book.all
+
+    respond_to do |format|
+      format.html
+      format.js
+      format.xml { render xml: @books }
+    end
+  end
 
 end
